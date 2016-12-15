8ad2dc52aa13de3db8a1363f86a9e95a2a68712d
Naoki Endoh <naoki.endoh@gmail.com>
2016-12-15 19:58:41 +0900

    Add first test

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,15 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
+import Hello as Hello
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "Hello Test"
+        [ test "Hello" <|
+            \_ ->
+                Hello.hello
+                    |> Expect.equal "hello"
+        ]
