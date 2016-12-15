82f0fd5e9183f6e53ff92af021cd7e46347e1029
Naoki Endoh <naoki.endoh@gmail.com>
2016-12-15 20:16:23 +0900

    Add some tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -5,11 +5,26 @@ import Test exposing (..)
 import Hello as Hello
 
 
-suite : Test
-suite =
+helloTest : Test
+helloTest =
     describe "Hello Test"
         [ test "Hello" <|
             \_ ->
                 Hello.hello
                     |> Expect.equal "hello"
+        , test "olloH" <|
+            \_ ->
+                Hello.hello
+                    |> String.reverse
+                    |> Expect.equal "olleh"
+        ]
+
+
+listTest : Test
+listTest =
+    describe "List"
+        [ test "Empty List" <|
+            \_ ->
+                List.length []
+                    |> Expect.equal 0
         ]
